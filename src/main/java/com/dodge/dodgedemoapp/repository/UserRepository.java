4203a0966b3375979e709b5c912fdda6e018a58a
Muthekar <BMuthekar@ad.harman.com>
2018-04-30 17:40:06 +0530

    Removed unused imports

--- src/main/java/com/dodge/dodgedemoapp/repository/UserRepository.java
+++ src/main/java/com/dodge/dodgedemoapp/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.dodge.dodgedemoapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.dodge.dodgedemoapp.entity.User;
 
